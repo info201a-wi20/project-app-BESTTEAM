8c7ebaaa851a3f6dbfc76b9c3bd71f6ead6b5c43
Andrew Liu <andrewliu@Andrews-MacBook-Pro-2.local>
2020-03-12 15:07:59 -0700

    deprecate using api

--- GetData.R
+++ GetData.R
@@ -7,18 +7,8 @@ library(jsonlite)
 # Usage: f: from date "yyyy-mm-dd", t: to date "yyyy-mm-dd", country_code:
 # country code of country ("US", "CN")
 getStock <- function(f, t, country_code) {
-  query = list(region = country_code, lang = "en", symbol = "NBEV", from = toString(as.numeric(as.POSIXct(f))), to = toString(as.numeric(as.POSIXct(t))), events = "div", interval = "1d")
-  res = GET("https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/get-histories", add_headers("X-RapidAPI-Host" = "apidojo-yahoo-finance-v1.p.rapidapi.com", "X-RapidAPI-Key" = "d83c0bf332mshff17063a7353c72p1c419cjsn43de5782fd8d"), query = query)
-  c = content(res, "text")
-  c = fromJSON(c)
-  t <- as.Date(as.POSIXct(c$chart$result[[1]]$timestamp, origin="1970-01-01"))
-  l <- c$chart$result[[1]]$indicators$quote[[1]]$low[[1]]
-  v <- c$chart$result[[1]]$indicators$quote[[1]]$volume[[1]]
-  cl <- c$chart$result[[1]]$indicators$quote[[1]]$close[[1]]
-  o <- c$chart$result[[1]]$indicators$quote[[1]]$open[[1]]
-  h <- c$chart$result[[1]]$indicators$quote[[1]]$high[[1]]
-  df <- data.frame(t, l, v, cl, o, h)
-  colnames(df) <- c("Date", "low", "volume", "close", "open", "high")
+  # use of api deprecated
+  df <- read.csv(paste0(getwd(),"/data/stock.csv"))
   return(df)
 }
 
